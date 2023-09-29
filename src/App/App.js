201c342f39f7eaf3917c496be9fc21c22ad45380
Olga Uk <olgablackangel@bk.ru>
2023-09-29 17:21:08 +0300

    верстка

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
-// import Main from '../Main/Main';
+import Main from '../Main/Main';
 // import Footer from '../Footer/Footer';
 
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      {/* <Main></Main>
-      <Footer></Footer> */}
+      <Main></Main>
+      {/* <Footer></Footer>  */}
     </div>
   );
 }
